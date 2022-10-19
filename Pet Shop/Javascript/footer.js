ff828d55d8d451d9ad00e0f65d296f237e0f23c2
Kei <keila.aranda@hotmail.com>
2022-10-19 18:34:24 -0300

    19/10 actu

---
+++ Pet Shop/Javascript/footer.js
@@ -0,0 +1,11 @@
+document.write(`<footer>
+<nav class="mapacuadro">
+    <h4>Dirección</h4>
+    <p>Av. Corrientes 2937, Buenos Aires</p>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d488.168473131593!2d-58.40747904803529!3d-34.60433971098559!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca8d48d408d5%3A0xd837a6cfdaa8f596!2sAv.%20Corrientes%202937%2C%20C1193%20AAB%2C%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1664846753253!5m2!1ses-419!2sar"
+        width="700" height="100" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+</nav>
+</footer>`)
---
+++ Pet Shop/Javascript/header.js
@@ -0,0 +1,10 @@
+document.write(`<header>
+    <h1>Pet Code</h1>
+</header>
+
+<nav class="menu">
+    <a href="index.html">Menu</a>
+    <a href="Turnos.html">Turnos</a>
+    <a href="PetShop.html">Pet Shop</a>
+    <a href="Adopcion.html">Adopcion</a>
+</nav>`)
